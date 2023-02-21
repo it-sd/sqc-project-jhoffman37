25bda9a05d63bc8f91380283050d2da284c282b0
jhoffman37 <jhoffman37@student.cvtc.edu>
2023-02-20 19:30:53 -0600

    correct database again

--- docs/schema.sql
+++ docs/schema.sql
@@ -2,29 +2,27 @@ Drop table if exists shelters;
 Drop table if exists pets;
 
 CREATE TABLE shelters (
-    shelterID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    totalPets int DEFAULT NULL,
-    petID int DEFAULT NULL,
-    Address VARCHAR(25) DEFAULT NULL,
-    City VARCHAR(25) DEFAULT NULL,
-    State VARCHAR(2) DEFAULT NULL,
-    Zip VARCHAR(5) DEFAULT NULL,
-    PRIMARY KEY (shelterID)
-)  AUTO_INCREMENT=1;
+    shelter_id INT PRIMARY KEY,
+    total_pets int DEFAULT NULL,
+    pet_id int DEFAULT NULL,
+    address VARCHAR(25) DEFAULT NULL,
+    city VARCHAR(25) DEFAULT NULL,
+    state VARCHAR(2) DEFAULT NULL,
+    zip VARCHAR(5) DEFAULT NULL,
+);
 
 
 CREATE TABLE pets (
-    petID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    petID PRIMARY KEY,
     name VARCHAR(25) DEFAULT NULL,
     gender varchar(6) DEFAULT NULL,
     age INT DEFAULT NULL,
     sizeLbs INT DEFAULT NULL,
     breed varchar(25) DEFAULT NULL,
     species varchar(25) DEFAULT NULL,
-    PRIMARY KEY (DeviceID)
-)  AUTO_INCREMENT=1;
+);
 
-INSERT INTO shelters (Address) VALUES
+INSERT INTO shelters (address) VALUES
 ('1519 Something Ave');
 
 -- Insert values into devices table for Device
