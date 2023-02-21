be4d1d552019b99b3adec38c8f16ef7733b00889
jhoffman37 <jhoffman37@student.cvtc.edu>
2023-02-20 19:37:15 -0600

    correct database a third time

--- docs/schema.sql
+++ docs/schema.sql
@@ -2,24 +2,24 @@ Drop table if exists shelters;
 Drop table if exists pets;
 
 CREATE TABLE shelters (
-    shelter_id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     total_pets int DEFAULT NULL,
     pet_id int DEFAULT NULL,
     address VARCHAR(25) DEFAULT NULL,
     city VARCHAR(25) DEFAULT NULL,
     state VARCHAR(2) DEFAULT NULL,
-    zip VARCHAR(5) DEFAULT NULL,
+    zip VARCHAR(5) DEFAULT NULL
 );
 
 
 CREATE TABLE pets (
-    petID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(25) DEFAULT NULL,
     gender varchar(6) DEFAULT NULL,
     age INT DEFAULT NULL,
     sizeLbs INT DEFAULT NULL,
     breed varchar(25) DEFAULT NULL,
-    species varchar(25) DEFAULT NULL,
+    species varchar(25) DEFAULT NULL
 );
 
 INSERT INTO shelters (address) VALUES
